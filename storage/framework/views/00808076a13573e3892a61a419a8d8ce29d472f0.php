351f594d7269b1348f01eb17f226d36812dc74fd
Christian Braybrooke <chris@purplemountmedia.com>
2018-09-05 12:15:28 +0100

    fixed untracked files

--- storage/framework/views/00808076a13573e3892a61a419a8d8ce29d472f0.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <items-create-table />
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('app.layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/4033c76be026e93ebf29ab308b5cc0fe8f761068.php
+++
@@ -1,6 +0,0 @@
-<?php if(auth()->guard()->guest()): ?>
-  <a href="<?php echo e(route('login')); ?>">Login</a>
-
-<?php else: ?>
-  <a href="<?php echo e(route('dashboard.index')); ?>">Dashboard</a>
-<?php endif; ?>
